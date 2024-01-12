d9ec1a5bef029df5781f9f2daeb701865e243cc8
Abhishek Biradar <abhishekbiradar0207@gmail.com>
2024-01-12 18:19:26 +0530

    Birthday Wisher using SMTPlib

---
+++ Birthday_wisher/main.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+import pandas
+import smtplib
+
+MY_EMAIL = "eren.jaeger2387@gmail.com"
+MY_PASSWORD = "@erwin12"
+
+today_tuple = (datetime.now().month, datetime.now().day)
+
+data = pandas.read_csv("Birthday_wisher/birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[name]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        )
